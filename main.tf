761237318194a2c11037f97cb72ec9bb2c50f938
henrybarnacle <henrybarnacle@yahoo.com>
2024-10-08 21:42:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,13 +52,21 @@ resource "aws_lambda_function" "lambda" {
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.handler"
   runtime       = "nodejs18.x"
-  filename      = "lambda_function_payload.zip"
 
   environment {
     variables = {
       STAGE = "dev"
     }
   }
+  source_code = <<EOF
+    exports.handler = async (event) => {
+      console.log("Hello, World!");
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello, World!" }),
+      };
+    };
+  EOF
 }
 
 # API Gateway

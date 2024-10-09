c54e216a76a22986f4eed3f014605dc33eac01b2
henrybarnacle <henrybarnacle@yahoo.com>
2024-10-09 08:48:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,6 +53,7 @@ resource "aws_lambda_function" "lambda" {
   handler       = "index.handler"
   runtime       = "nodejs18.x"
   filename      = "lambda_function_payload.zip"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
   environment {
     variables = {

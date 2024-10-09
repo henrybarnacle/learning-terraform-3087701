1347823738686cb41a12db956c629821c91c2bc9
henrybarnacle <henrybarnacle@yahoo.com>
2024-10-09 09:48:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "aws_iam_policy" "lambda_s3_access_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
-  role       = aws_iam_role.lambda_execution_role.name
+  role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.lambda_s3_access_policy.arn
 }
 

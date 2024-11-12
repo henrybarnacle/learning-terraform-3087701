1e9b540d4717d1cfcf503e2011dea3794fbdfec6
henrybarnacle <henrybarnacle@yahoo.com>
2024-11-12 11:00:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -77,11 +77,11 @@ resource "aws_api_gateway_resource" "proxy" {
   path_part   = "getRates"
 }
 
-# ANY method
+# POST method
 resource "aws_api_gateway_method" "proxy_method" {
   rest_api_id   = aws_api_gateway_rest_api.rates-api.id
   resource_id   = aws_api_gateway_resource.proxy.id
-  http_method   = "GET"
+  http_method   = "POST"
   authorization = "NONE"
 }
 

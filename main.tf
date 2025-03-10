59e9edfae6e4698f9efb26f857c12c9e2935f121
henrybarnacle <henrybarnacle@yahoo.com>
2025-03-10 13:39:12 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_api_gateway_resource" "proxy" {
 }
 
 # POST method
-resource "aws_api_gateway_method" "post_method" {
+resource "aws_api_gateway_method" "proxy_method" {
   rest_api_id   = aws_api_gateway_rest_api.rates-api.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "POST"
@@ -78,7 +78,7 @@ resource "aws_api_gateway_method" "post_method" {
 resource "aws_api_gateway_method_response" "post_response" {
   rest_api_id = aws_api_gateway_rest_api.rates-api.id
   resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.post_method.http_method
+  http_method = aws_api_gateway_method.proxy_method.http_method
   status_code = "200"
 
   response_parameters = {
@@ -90,7 +90,7 @@ resource "aws_api_gateway_method_response" "post_response" {
 resource "aws_api_gateway_integration" "post_integration" {
   rest_api_id = aws_api_gateway_rest_api.rates-api.id
   resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.post_method.http_method
+  http_method = aws_api_gateway_method.proxy_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.xx-lambda.invoke_arn
@@ -98,7 +98,7 @@ resource "aws_api_gateway_integration" "post_integration" {
 resource "aws_api_gateway_integration_response" "post_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.rates-api.id
   resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.post_method.http_method
+  http_method = aws_api_gateway_method.proxy_method.http_method
   status_code = "200"
 
   response_parameters = {
@@ -166,7 +166,7 @@ resource "aws_api_gateway_method_response" "options_method_response" {
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.rates-api.id
   resource_id             = aws_api_gateway_resource.proxy.id
-  http_method             = aws_api_gateway_method.post_method.http_method
+  http_method             = aws_api_gateway_method.proxy_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.xx-lambda.invoke_arn

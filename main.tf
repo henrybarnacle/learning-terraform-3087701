44d95ac95e202f344007da9f428ba93eb481435c
henrybarnacle <henrybarnacle@yahoo.com>
2025-03-10 13:15:36 -0400

    xx-lambda

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "aws_api_gateway_integration" "post_integration" {
   http_method = aws_api_gateway_method.post_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.aspida_rates.invoke_arn
+  uri                     = aws_lambda_function.xx-lambda.invoke_arn
 }
 resource "aws_api_gateway_integration_response" "post_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.rates-api.id

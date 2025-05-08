d0fb0a3cafa190d2917272e454f708def80e1c67
henrybarnacle <henrybarnacle@yahoo.com>
2025-05-08 12:24:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -84,6 +84,7 @@ resource "aws_api_gateway_integration" "get_integration" {
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.xx-lambda.invoke_arn
 }
+
 resource "aws_api_gateway_method_response" "get_method_response" {
   rest_api_id = aws_api_gateway_rest_api.rates-api.id
   resource_id = aws_api_gateway_resource.proxy.id
@@ -91,10 +92,15 @@ resource "aws_api_gateway_method_response" "get_method_response" {
   status_code = "200"
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Origin" = true
   }
 }
+
 resource "aws_api_gateway_integration_response" "get_integration_response" {
+  depends_on = [
+    aws_api_gateway_method_response.get_method_response
+  ]
+
   rest_api_id = aws_api_gateway_rest_api.rates-api.id
   resource_id = aws_api_gateway_resource.proxy.id
   http_method = aws_api_gateway_method.get_method.http_method
